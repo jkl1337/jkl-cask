3d343221e78ebfb1dc3965a5a249dc18fa081bed
John K. Luebs <jkl@johnluebs.com>
2014-03-22 02:39:13 -0400

    Update realgud

---
+++ 24.3.1/elpa/realgud-20140321.2014/realgud-autoloads.el
@@ -0,0 +1,18 @@
+;;; realgud-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("realgud-pkg.el" "realgud.el") (21293
+;;;;;;  12159 983252 641000))
+
+;;;***
+
+(provide 'realgud-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; realgud-autoloads.el ends here
