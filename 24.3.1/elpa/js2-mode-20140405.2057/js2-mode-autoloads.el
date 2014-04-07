ed35d21882a2dcdfe1bf52f523d0deca5e9087fe
John K. Luebs <jkl@johnluebs.com>
2014-04-07 02:36:13 -0400

    Updated packages: org-plus-contrib 20140331 -> 20140407 magit 20140403.929 -> 20140406.1218 js2-mode 20140326.420 -> 20140405.2057

---
+++ 24.3.1/elpa/js2-mode-20140405.2057/js2-mode-autoloads.el
@@ -0,0 +1,54 @@
+;;; js2-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (js2-imenu-extras-mode js2-imenu-extras-setup)
+;;;;;;  "js2-imenu-extras" "js2-imenu-extras.el" (21314 18227 673113
+;;;;;;  30000))
+;;; Generated autoloads from js2-imenu-extras.el
+
+(autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'js2-imenu-extras-mode "js2-imenu-extras" "\
+Toggle Imenu support for frameworks and structural patterns.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (js2-mode js2-minor-mode) "js2-mode" "js2-mode.el"
+;;;;;;  (21314 18227 659779 462000))
+;;; Generated autoloads from js2-mode.el
+
+(autoload 'js2-minor-mode "js2-mode" "\
+Minor mode for running js2 as a background linter.
+This allows you to use a different major mode for JavaScript editing,
+such as `espresso-mode', while retaining the asynchronous error/warning
+highlighting features of `js2-mode'.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'js2-mode "js2-mode" "\
+Major mode for editing JavaScript code.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("js2-mode-pkg.el") (21314 18227 676693
+;;;;;;  485000))
+
+;;;***
+
+(provide 'js2-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; js2-mode-autoloads.el ends here
