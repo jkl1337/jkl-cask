66e08c04e5d74a407ed06ccbd1c286ccb3aba647
John K. Luebs <jkl@johnluebs.com>
2014-04-30 23:20:46 -0400

    Outdated packages: yasnippet 20140314.255 -> 20140427.1224 web-mode 20140421.641 -> 20140430.1535 smex 20140410.547 -> 20140425.1314 smartparens 20140414.606 -> 20140427.418 skewer-mode 20140410.1858 -> 20140430.839 projectile 20140416.1040 -> 20140428.205 pallet 20140413.1345 -> 20140430.1234 package-build 20140422.803 -> 20140427.357 org-plus-contrib 20140422 -> 20140428 inf-ruby 20140306.1941 -> 20140428.201 helm-swoop 20140404.2206 -> 20140424.2149 helm 20140424.801 -> 20140430.1204 company-go 20140221.607 -> 20140429.2147 company 20140419.313 -> 20140430.154 cider 20140424.748 -> 20140430.838

--- 24.3.1/elpa/helm-20140424.801/helm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm" "20140424.801" "Helm is an Emacs incremental and narrowing framework" 'nil)
--- 24.3.1/elpa/pallet-20140413.1345/pallet-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "pallet" "20140413.1345" "A package management tool for Emacs, using Cask."
-  '((cask "0.6")))
