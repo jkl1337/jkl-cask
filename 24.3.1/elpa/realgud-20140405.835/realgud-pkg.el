d533dd1046cf18a9ac78abbc85b632c8004d80a8
John K. Luebs <jkl@johnluebs.com>
2014-05-11 23:34:27 -0400

    Outdated packages: yasnippet 20140427.1224 -> 20140511.947 xcscope 20140330.2346 -> 20140510.1437 web-mode 20140505.1318 -> 20140511.1206 undo-tree 20140110.616 -> 20140509.522 smartparens 20140427.418 -> 20140510.1311 ruby-compilation 20140407.726 -> 20140508.246 realgud 20140405.835 -> 20140510.2344 projectile 20140428.205 -> 20140510.1105 nose 20140410.2024 -> 20140501.1740 magit 20140505.301 -> 20140508.1256 helm-ag 20140505.21 -> 20140507.2010 helm 20140505.2059 -> 20140511.432 go-eldoc 20140307.1711 -> 20140509.150 cider 20140505.1132 -> 20140510.1324 cask 20140324.15 -> 20140511.43

--- 24.3.1/elpa/realgud-20140405.835/realgud-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "realgud" "20140405.835" "A modular front-end for interacting with external debuggers"
-  '((load-relative "20130410")
-    (loc-changes "20130723")
-    (test-simple "20130710"))
-  :url "http://github.com/rocky/emacs-dbgr")
---
+++ 24.3.1/elpa/realgud-20140510.2344/realgud-pkg.el
@@ -0,0 +1,5 @@
+(define-package "realgud" "20140510.2344" "A modular front-end for interacting with external debuggers"
+  '((load-relative "20130410")
+    (loc-changes "20130723")
+    (test-simple "20130710"))
+  :url "http://github.com/rocky/emacs-dbgr")
