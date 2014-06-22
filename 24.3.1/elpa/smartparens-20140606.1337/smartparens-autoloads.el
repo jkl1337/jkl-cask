deb3080e183f42d9b51adf8d3a230b08c63707ad
John K. Luebs <jkl@johnluebs.com>
2014-06-22 01:06:08 -0400

    Add smartparens

--- 24.3.1/elpa/smartparens-20140606.1337/smartparens-autoloads.el
+++ 24.3.1/elpa/smartparens-20140606.1337/smartparens-autoloads.el
@@ -8,7 +8,7 @@
 ;;;;;;  turn-on-smartparens-mode smartparens-global-mode turn-on-smartparens-strict-mode
 ;;;;;;  smartparens-global-strict-mode smartparens-strict-mode smartparens-mode
 ;;;;;;  sp-use-smartparens-bindings sp-use-paredit-bindings sp-cheat-sheet)
-;;;;;;  "smartparens" "smartparens.el" (21403 18566 349004 408000))
+;;;;;;  "smartparens" "smartparens.el" (21414 25646 821827 456000))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -173,7 +173,7 @@ Turn off `show-smartparens-mode'.
 
 ;;;### (autoloads nil nil ("smartparens-config.el" "smartparens-html.el"
 ;;;;;;  "smartparens-latex.el" "smartparens-lua.el" "smartparens-pkg.el"
-;;;;;;  "smartparens-ruby.el") (21403 18566 361374 59000))
+;;;;;;  "smartparens-ruby.el") (21414 25646 851846 796000))
 
 ;;;***
 
