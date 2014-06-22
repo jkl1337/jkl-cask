d8af2c017197f41882775d78962f05fe6c460f6c
John K. Luebs <jkl@johnluebs.com>
2014-06-22 01:03:51 -0400

    Outdated packages: yasnippet 20140514.1649 -> 20140617.1640 web-mode 20140612.2322 -> 20140620.1426 skewer-mode 20140430.839 -> 20140614.830 s 20131223.944 -> 20140620.1657 rust-mode 20140512.2145 -> 20140618.536 ruby-compilation 20140508.246 -> 20140620.217 rspec-mode 20140605.2133 -> 20140617.936 pyvenv 20140609.722 -> 20140621.1017 projectile 20140607.108 -> 20140617.29 package-build 20140610.1100 -> 20140617.235 org-plus-contrib 20140613 -> 20140619 magit 20140609.810 -> 20140619.530 helm-ag 20140610.1906 -> 20140615.209 helm 20140609.136 -> 20140621.23 google-this 20131031.315 -> 20140619.1011 go-mode 20140409.928 -> 20140617.1252 flycheck 20140612.1242 -> 20140618.13 epl 20140610.631 -> 20140620.218 emmet-mode 20140611.311 -> 20140613.2306 dash 20140607.33 -> 20140618.2212 company 20140613.850 -> 20140614.2104 clojure-mode 20140613.856 -> 20140617.508 cider 20140613.736 -> 20140620.753 auto-complete 20140605.1908 -> 20140618.2217

--- 24.3.1/elpa/epl-20140610.631/epl-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21403 20208 329533
-;;;;;;  483000))
-
-;;;***
-
-(provide 'epl-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; epl-autoloads.el ends here
