5456c6aed9f5639d7b00b57cb890100f9f1ff09b
John K. Luebs <jkl@johnluebs.com>
2015-02-05 18:43:51 -0500

    Update

--- 24.4.1/elpa/realgud-20150110.1628/realgud-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; realgud-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("realgud-pkg.el" "realgud.el") (21682
-;;;;;;  48647 766999 376000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; realgud-autoloads.el ends here
--- 24.4.1/elpa/rust-mode-20150107.735/rust-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rust-mode" "20150107.735" "A major emacs mode for editing Rust source code" 'nil :url "https://github.com/rust-lang/rust" :keywords '("languages"))
