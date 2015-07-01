dd4163172d5cc371a773ae7a90e8fd884d339f28
John K. Luebs <jkl@johnluebs.com>
2015-07-01 12:26:28 -0400

    Outdated packages: web-mode 20150612.1118 -> 20150630.2301 smartparens 20150603.948 -> 20150628.1418 scala-mode2 20150113.2309 -> 20150617.2350 rspec-mode 20150521.822 -> 20150629.1524 projectile 20150610.2247 -> 20150701.121 popup 20150609.2145 -> 20150626.711 php-mode 20150602.2318 -> 20150623.431 org-plus-contrib 20150615 -> 20150629 markdown-mode 20150606.2129 -> 20150628.502 js2-mode 20150524.426 -> 20150630.1336 ido-ubiquitous 20150528.2322 -> 20150630.926 ido-completing-read+ 20150528.2322 -> 20150630.926 helm-projectile 20150610.253 -> 20150621.1322 helm-ag 20150616.1645 -> 20150630.318 helm 20150617.545 -> 20150701.117 flycheck 20150612.235 -> 20150626.1527 dash 20150611.922 -> 20150701.20 company 20150616.439 -> 20150701.410 cmake-mode 20150120.620 -> 20150625.739 clojure-mode 20150614.1628 -> 20150630.1410 cider 20150616.1226 -> 20150630.1406 auto-complete 20150615.34 -> 20150618.1949 anaconda-mode 20150313.655 -> 20150626.441

---
+++ 24.5.1/elpa/clojure-mode-20150630.1410/clojure-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode" "20150630.1410" "Major mode for Clojure code" '((emacs "24.3")) :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
